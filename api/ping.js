0fdcf14dccb6e06c30d89f764e7e00d3eed44b33
pierrechaligne14-creator <pierrechaligne14@gmail.com>
2026-07-17 11:10:03 +0200

    Change Supabase URL in ping.js
    Updated the Supabase URL in the client creation.

--- api/ping.js
+++ api/ping.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const db = createClient(
-  process.env.SUPABASE_URL,
+  process.env.https://dmswinbptxrosuccxapr.supabase.co,
   process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtc3dpbmJwdHhyb3N1Y2N4YXByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MzEwMTEsImV4cCI6MjA4OTQwNzAxMX0.AodlEsQr96FYtq0RTxMpo2j6dvZBdAtRZJ5-Pnh-WC8
 );
 
